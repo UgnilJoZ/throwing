db73439b3dc3c91144ba38e96097f050fa3e54a0
Joz <joz@spline.de>
2016-10-09 22:31:12 +0000

    Fixes

--- megatnt_arrow.lua
+++ megatnt_arrow.lua
@@ -1,5 +1,5 @@
 minetest.register_craftitem("throwing:megatnt_arrow", {
-	description = "Arrow",
+	description = "Mega TNT Arrow",
 	inventory_image = "throwing_arrow.png",
 })
 
@@ -8,12 +8,12 @@ minetest.register_node("throwing:arrow_box", {
 	node_box = {
 		type = "fixed",
 		fixed = {
-			-- Shaft
+			-- Shaft / Stiel
 			{-6.5/17, -1.5/17, -1.5/17, 6.5/17, 1.5/17, 1.5/17},
-			--Spitze
+			--Spitze / Arrow Head
 			{-4.5/17, 2.5/17, 2.5/17, -3.5/17, -2.5/17, -2.5/17},
 			{-8.5/17, 0.5/17, 0.5/17, -6.5/17, -0.5/17, -0.5/17},
-			--Federn
+			--Federn / Feather
 			{6.5/17, 1.5/17, 1.5/17, 7.5/17, 2.5/17, 2.5/17},
 			{7.5/17, -2.5/17, 2.5/17, 6.5/17, -1.5/17, 1.5/17},
 			{7.5/17, 2.5/17, -2.5/17, 6.5/17, 1.5/17, -1.5/17},
@@ -31,7 +31,6 @@ minetest.register_node("throwing:arrow_box", {
 
 local THROWING_ARROW_ENTITY={
 	physical = false,
-	timer=0,
 	visual = "wielditem",
 	visual_size = {x=0.1, y=0.1},
 	textures = {"throwing:arrow_box"},
@@ -40,34 +39,8 @@ local THROWING_ARROW_ENTITY={
 }
 
 THROWING_ARROW_ENTITY.on_step = function(self, dtime)
-	self.timer=self.timer+dtime
 	local pos = self.object:getpos()
 	local node = minetest.env:get_node(pos)
-
-	if self.timer>0.2 then
-		local objs = minetest.env:get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 2)
-		for k, obj in pairs(objs) do
-			if obj:get_luaentity() ~= nil then
-				if obj:get_luaentity().name ~= "throwing:arrow_entity" and obj:get_luaentity().name ~= "__builtin:item" then
-					local damage = 3
-					obj:punch(self.object, 7.0, {
-						full_punch_interval=1.0,
-						damage_groups={fleshy=damage},
-					}, nil)
-					self.object:remove()
-					minetest.set_node(pos, {name="supertnt:megatnt_burning"})
-				end
-			else
-				local damage = 100
-				obj:punch(self.object, 1.0, {
-					full_punch_interval=1.0,
-					damage_groups={fleshy=damage},
-				}, nil)
-				self.object:remove()
-				minetest.set_node(pos, {name="supertnt:megatnt_burning"})
-			end
-		end
-	end
 
 	-- Not the first on_step call?
 	if self.lastpos.x~=nil then
